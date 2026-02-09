913f1b3655c9472a4b6b1b1cc3c3d1cdde577c7d
cvrain <cvraindays@outlook.com>
2026-02-09 11:30:46 +0800

    添加新分支,增加example代码

---
+++ example/main.cpp
@@ -0,0 +1,23 @@
+#include <QCoreApplication>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[]) {
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url(u"qrc:/qt/qml/NandinaExample/Main.qml"_qs);
+
+    QObject::connect(
+            &engine, &QQmlApplicationEngine::objectCreated, &app,
+            [url](QObject *obj, const QUrl &objUrl) {
+                if (!obj && url == objUrl) {
+                    QCoreApplication::exit(-1);
+                }
+            },
+            Qt::QueuedConnection);
+
+    engine.loadFromModule("NandinaExample", "Main");
+
+    return app.exec();
+}
